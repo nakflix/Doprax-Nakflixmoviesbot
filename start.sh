5848981881de59a1a0e67ab41351068f538166f1
nakflix <123237114+nakflix@users.noreply.github.com>
2023-02-22 14:36:03 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone https://github.com/lazygramyt/File-Sharing-Bot ok && cd ok && pip3 install -U -r requirements.txt
-python3 main.py
+git clone https://github.com/nakflix/UPDATE ok && cd ok && pip3 install -U -r requirements.txt
+python3 bot.py
